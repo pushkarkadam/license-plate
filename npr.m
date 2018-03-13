52de7019ad80185b151af35722ebd7d5353b90a4
Pushkar Kadam <pushkarkadam17@outlook.com>
2018-03-13 23:10:43 +1100

    code rearranged, variable names changed, unnecessary code removed

--- npr.m
+++ npr.m
@@ -1,16 +1,9 @@
-%% Number Plate Recognition code
+%% Number Plate Recognition function
 % Author: Pushkar Kadam
 % Date: 15/10/2017
 
 %% Importing image
-function plate_number =  npr(img)
-    %I = imread('subaru.jpeg');
-    % I = imread('newcar7.jpeg');
-    % I = imread('newcar3.jpeg');
-    % I = imread('toyota.jpeg');
-    % I = imread('newcar6.jpeg');
-    I = img;
-
+function plate_number =  npr(image)
     %% Importing the template images
     template = temp_read();
 
@@ -19,13 +12,13 @@ function plate_number =  npr(img)
     % error messages and automatically resizes the image to 50 percent
     % The image taken are of high resolution and therefore they are resized in
     % this code to 50 percent of its actual size.
-    I = imresize(I,[600 800]);
+    image = imresize(image,[600 800]);
 
     %% Converting Color image to Gray scale image
     % The color image obtained is usually a 3 dimensional matrix. Most of the
     % operations that are concerned with the image processing are performed on
     % 2 dimensional matrix.
-    Igray = rgb2gray(I);
+    gray_image = rgb2gray(image);
 
     %% Mathematical Morphology to detect edges
     % Mathematical morphology is used for edge detection
@@ -34,38 +27,29 @@ function plate_number =  npr(img)
     % The reason for using binary image is that it will later be used to
     % multiply with another binary image to obtained the characters of the
     % number plate.
-    thresh = graythresh(Igray);
-    Ibw = im2bw(Igray,thresh); %Code line for transforming image into binary
+    threshold_value = graythresh(gray_image);
+    binary_image = im2bw(gray_image,threshold_value); %Code line for transforming image into binary
 
     %% Edge detection using Sobel method
-    Iedge = edge(Igray,'sobel');
-    % figure;
-    % imshow(Iedge)
+    edge_image = edge(gray_image,'sobel');
 
     %% Code for dilating the image
     % Dilation of the image enhances the borders of the image
-    se = strel('square',3);
+    SE = strel('square',3);
 
-    Iedge2 = imdilate(Iedge, se);
-    % figure;
-    % imshow(Iedge2)
+    edge_image = imdilate(edge_image, SE);
 
     %% Filling the edges with white color
-
-    Ifill = imfill(Iedge2,'holes');
+    fill_image = imfill(edge_image,'holes');
 
     %% Removing the other edges that are not inside the number plate region
-    Inew = Ifill;
-    %Inew = imclearborder(Ifill);
-    % figure;
-    % imshow(Inew)
-
+    eroded_image = fill_image;
+    
     %% Using erode to get rid of unnecessary dots on the image
     % This command is used three times to get rid of most of the dots
-    I1 = imerode(Inew,se);
-    I2 = imerode(I1,se);
-    I3 = imerode(I2,se);
-    %imshow(I3)
+    for x = 1:3
+        eroded_image = imerode(eroded_image,SE);
+    end
 
     %% Getting the characters on the number plate
     % The binary image obtained after converting the B&W image is multiplied
@@ -75,9 +59,7 @@ function plate_number =  npr(img)
     % number plate were black in color therefore they gave the results of black
     % on white in the result of this image.
     % Always multiple two binary images.
-    PP = Ibw.*I3;
-    figure;
-    %imshow(PP)
+    character_image = binary_image.*eroded_image;
 
     %% Character segmentation
     % This is very much important.
@@ -96,24 +78,18 @@ function plate_number =  npr(img)
     % was not able to get rid of the white dot that exists between the number
     % plates. If we create a template for that dot, then maybe even that dot
     % can be identified.
-    rr = ~PP;
-    %imshow(rr)
-    mm = imclearborder(rr);
-    ss = imerode(mm,se);
-    ss1 = imerode(ss,se);
-    ss1 = imerode(ss1,se);
-    gg = imdilate(ss1,se);
-    gg = imdilate(gg,se);
-    gg = imdilate(gg,se);
-    gg = imdilate(gg,se);
-    ss1 = gg;
-    %imshow(ss1)
-    %% Close the gap
-    % If there are some gaps in the characters, then imclose can enable to
-    % close the gaps. It is a morphological function.
-    im_close = imclose(PP,se);
-    %imshow(im_close)
+    character_image = ~character_image;
+    character_image_erode = imclearborder(character_image);
+    
+    for x = 1:3
+        character_image_erode = imerode(character_image_erode,SE);
+    end
 
+    dilate_character_image = character_image_erode;
+    
+    for x = 1:4
+        dilate_character_image = imdilate(dilate_character_image,SE);
+    end
 
     %% Using Bounding Box
     % s is a structure where each element in this structure contains a
@@ -121,7 +97,7 @@ function plate_number =  npr(img)
     % BoundingBox property for each object is a 4 element array [x y w h] where
     % (x,y) denotes column and row co-ordinates of the upper left corner of the
     % bounding box and (w,h) denote the width and height of the BoundingBox
-    s = regionprops(ss1, 'BoundingBox');
+    structure = regionprops(dilate_character_image, 'BoundingBox');
 
     %% 4 column matrix
     % Creating 4 column matrix that encapsulate all of these BoundingBox
@@ -130,16 +106,15 @@ function plate_number =  npr(img)
     % letters from the image, we need to use the integer co-ordinates which is
     % the form of natural representation of the image.
 
-    bb = round(reshape([s.BoundingBox],4,[]).');
+    bb = round(reshape([structure.BoundingBox],4,[]).');
 
     %% Creating the bounding box rectangles
     % numel(s) returns the total number of elements that are present in that
     % matrix. It can be equivalent to using prod(size(s))
     %imshow(ss1)
-    for idx = 1:numel(s)
+    for idx = 1:numel(structure)
         rectangle('Position', bb(idx,:),'edgecolor','red');
     end
-
 
     %% Extracting the characters to a cell
     % Extracting all the characters and placing them in the cell array. The
@@ -148,27 +123,18 @@ function plate_number =  npr(img)
     % different sizes.
     % By simply looping over every bounding box, bounding box pixels can be
     % extracted to each character and it can be placed into a cell array.
-    chars = cell(1,numel(s));
-    for idx = 1:numel(s)
-        chars{idx} = ss1(bb(idx,2):bb(idx,2)+bb(idx,4)-1, bb(idx,1):bb(idx,1)+bb(idx,3)-1);
+    chars = cell(1,numel(structure));
+    for idx = 1:numel(structure)
+        chars{idx} = dilate_character_image(bb(idx,2):bb(idx,2)+bb(idx,4)-1, bb(idx,1):bb(idx,1)+bb(idx,3)-1);
     end
 
     %% Resizing all the images
     % Resizing is essential for template matching
-    for idx = 1:numel(s)
+    for idx = 1:numel(structure)
         chars{idx} = imresize(chars{idx}, [57 42]);
     end
-
-    %% Showing each image obtained
-    % By looping through each element of the cell array, the image can be
-    % viewed of separate characters.
-    % for idx = 1:numel(s)
-    %     figure
-    %     imshow(chars{idx})
-    % end
-
+    
     %% Character recognition code
-
     plate_number = char_reg(template,chars);
 
     %% Stores the license plate text in a txt file
