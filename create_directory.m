92f59c5b75b588d0cabdd651e6481a9e87f1d238
Pushkar Kadam <pushkarkadam17@outlook.com>
2018-04-21 21:52:34 +1000

    main function removed and text storage functioned removed

--- create_directory.m
+++ create_directory.m
@@ -2,7 +2,12 @@ function create_directory(license_plate, front_image, left_image, right_image)
     mkdir(license_plate);
     cd(license_plate);
     % Storing the license plate number in txt file
-    text_storage(license_plate);
+    fid = fopen('license.txt','wt');
+    fprintf(fid,'-----------------------------------\n');
+    fprintf(fid,'License plate number: ');
+    fprintf(fid,license_plate);
+    fprintf(fid,'\n-----------------------------------\n');
+    fclose(fid);
     % Storing images in the directory
     imwrite(front_image,'front_image.jpeg');
     imwrite(left_image,'left_image.jpeg');
--- main.m
+++ main.m
@@ -1,33 +1,32 @@
-function main()
-    %% Creating Arduino Object
-    arduino_board = arduino('COM38','Uno','Libraries','JRodrigoTech/HCSR04');
-    
-    %% Creating Sensor object
-    sensor = addon(arduino_board, 'JRodrigoTech/HCSR04','D12','D13');
-    
-    %% Main License plate recognition algorithm
-    threshold_distance = 4; % Distance from the sensor to the object in inches
-    
-    while true
-        sensor_distance = readDistance(sensor);
-        fprinf('Current distance is %.4f meters\n',sensor_distance);
-        
-        sensor_distance = convlenght(sensor_distance,'m','in');
-        
-        if (sensor_distance <= threshold_distance)
-            % Capturing the images from the cameras
-            front_image = image_capture('USB Camera #2');
-            left_image = image_capture('USB Camera #3');
-            right_image = image_capture('USB Camera #4');
-            
-            % License plate recognition sub-routine
-            license_plate = npr(front_image);
-            
-            % Directory sub-routine
-            create_directory(license_plate, front_image, left_image, right_image);
-            break;
-        else
-            continue;
-        end
+%% Creating Arduino Object
+%arduino_board = arduino('COM38','Uno','Libraries','JRodrigoTech/HCSR04');
+arduino_board = arduino();
+
+%% Creating Sensor object
+sensor = addon(arduino_board, 'JRodrigoTech/HCSR04','D12','D13');
+
+%% Main License plate recognition algorithm
+threshold_distance = 0.05; % Distance from the sensor to the object in metres
+
+while true
+    sensor_distance = readDistance(sensor);
+    fprintf('Current distance is %.4f meters\n',sensor_distance);
+
+    %sensor_distance = convlength(sensor_distance,'m','in');
+
+    if (sensor_distance <= threshold_distance)
+        % Capturing the images from the cameras
+        front_image = image_capture('USB Camera #2');
+        left_image = image_capture('USB Camera #3');
+        right_image = image_capture('USB Camera #4');
+
+        % License plate recognition sub-routine
+        license_plate = npr(front_image);
+
+        % Directory sub-routine
+        create_directory(license_plate, front_image, left_image, right_image);
+        break;
+    else
+        continue;
     end
 end
